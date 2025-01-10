48a9d2276be27faaea74733ef08d2ff580603a9b
Anna Drobysh <ann.drobysh@yandex.by>
2025-01-10 16:35:35 +0100

    feat: implement findCommonElements function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -540,8 +540,14 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const arrayReturn = arr2.map((element) => {
+    if (arr1.includes(element)) {
+      return element;
+    }
+    return undefined;
+  });
+  return arrayReturn.filter((x) => x !== undefined);
 }
 
 /**
