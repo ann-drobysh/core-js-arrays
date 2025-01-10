7bdcc44d9df1c7a612c9fab57940f5a77ff7a326
Anna Drobysh <ann.drobysh@yandex.by>
2025-01-10 14:05:32 +0100

    feat: implement getIndicesOfOddNumbers function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -478,8 +478,14 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const arr = numbers.map((x, i) => {
+    if (x % 2 !== 0) {
+      return i;
+    }
+    return undefined;
+  });
+  return arr.filter((x) => x !== undefined);
 }
 
 /**
